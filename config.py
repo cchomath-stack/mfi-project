703a17bd6a164e4a414cb3b180c518603f7fcda8
sungmin <cchomath@gmail.com>
2026-02-12 00:45:25 +0900

    fix: correct GOOGLE_REDIRECT_URI and prepare for production deployment

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 # 클라우드 배포 시에는 서버 환경 변수에서 값을 가져옵니다.
 GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", "892646063722-nf6ja53jafafipqgk72qlrvlgkj0qjp1.apps.googleusercontent.com")
 GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", "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")
-GOOGLE_REDIRECT_URI = os.environ.get("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/callback")
+GOOGLE_REDIRECT_URI = os.environ.get("GOOGLE_REDIRECT_URI", "http://localhost:8000/auth/google/callback")
 
 # JWT Settings
 SECRET_KEY = os.environ.get("SECRET_KEY", "antigravity_secret_key_change_in_production")
